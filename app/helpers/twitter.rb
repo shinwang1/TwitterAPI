6f65c89a7f48bfd5c45f7966a853845219067812
Shin <mrmaxin@gmail.com>
2016-06-06 00:09:28 -0700

    add tweeting helper, controller, and simple form

---
+++ app/helpers/twitter.rb
@@ -0,0 +1,9 @@
+def tweet(t)
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.access_token = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  end
+  client.update(t)
+end
