11714b2c97ac8324d109c829a6cb6466d4b76bb5
Shin <mrmaxin@gmail.com>
2016-06-06 22:48:09 -0700

    add method for searching recent tweets

--- app/helpers/twitter.rb
+++ app/helpers/twitter.rb
@@ -7,3 +7,17 @@ def tweet(t)
   end
   client.update(t)
 end
+
+def tweet_search(t)
+  tweet_array = []
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.access_token = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  end
+  client.search(t, result_type: "recent").take(5).each do |tweet|
+    tweet_array << tweet.text
+  end
+  tweet_array
+end
